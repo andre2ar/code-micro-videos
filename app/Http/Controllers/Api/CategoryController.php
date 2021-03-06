ebe0fc56447450c536a73eaff338156dd2a588a7
André Alvim Ribeiro <andre2ar@outlook.com>
2021-03-06 11:45:10 -0300

    Categories

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    private $rules = [
+        'name' => 'required|max:255',
+        'is_active' => 'boolean'
+    ];
+
+    public function index()
+    {
+        return Category::all();
+    }
+
+    public function store(Request $request): Category
+    {
+        $request->validate($this->rules);
+
+        return Category::create($request->all());
+    }
+
+    public function show(Category $category): Category
+    {
+        return $category;
+    }
+
+    public function update(Request $request, Category $category): Category
+    {
+        $request->validate($this->rules);
+        $category->update($request->all());
+
+        return $category;
+    }
+
+    public function destroy(Category $category)
+    {
+        $category->delete();
+        return response()->noContent();
+    }
+}
