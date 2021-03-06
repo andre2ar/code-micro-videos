6b131fc192da25dae40fb66936fff76c599292c0
André Alvim Ribeiro <andre2ar@outlook.com>
2021-03-06 14:24:39 -0300

    Categories

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -2,15 +2,20 @@
 
 namespace App\Models;
 
+use App\Models\Traits\Uuid;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes, Uuid;
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $fillable = [
         'name',
         'description',
         'is_active'
     ];
+    protected $dates = ['deleted_at'];
 }
