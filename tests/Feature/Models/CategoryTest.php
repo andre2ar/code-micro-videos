a6688a722b3e9a2e46f13034d310f5071b186261
André Alvim Ribeiro <andre2ar@outlook.com>
2021-03-14 11:22:49 -0300

    Category
    Signed-off-by: André Alvim Ribeiro <andre2ar@outlook.com>

---
+++ tests/Feature/Models/CategoryTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature\Models;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class CategoryTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testList()
+    {
+        Category::factory(1)->create();
+        $categories = Category::all();
+
+        $this->assertCount(1, $categories);
+
+        $categoryKeys = array_keys($categories->first()->getAttributes());
+        $this->assertEqualsCanonicalizing([
+            'id', 'name', 'description', 'is_active', 'created_at', 'updated_at', 'deleted_at'
+        ], $categoryKeys);
+    }
+}
